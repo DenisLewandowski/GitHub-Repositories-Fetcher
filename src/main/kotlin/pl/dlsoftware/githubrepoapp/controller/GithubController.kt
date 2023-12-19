d493cc20438c7455f768a30b1444b9f458af6783
dlewandowski <denis.lewandowski@rst.com.pl>
2023-12-19 18:55:29 +0100

    handling response for not supported media types

--- src/main/kotlin/pl/dlsoftware/githubrepoapp/controller/GithubController.kt
+++ src/main/kotlin/pl/dlsoftware/githubrepoapp/controller/GithubController.kt
@@ -1,6 +1,8 @@
 package pl.dlsoftware.githubrepoapp.controller
 
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
+import org.springframework.web.HttpMediaTypeNotAcceptableException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -17,13 +19,23 @@ class GithubController(
     private val gitHubService: GitHubService
 ) {
 
-    @GetMapping("/repos/{username}", produces = ["application/json"])
+    @GetMapping("/repos/{username}")
     fun getUserGithubRepositories(@PathVariable username: String): ResponseEntity<UserRepositoriesResponse> {
         return ResponseEntity.ok(gitHubService.getRepositoriesByUsernameNoForks(username))
     }
 
     @ExceptionHandler
     fun handleGitHubHttpRequestFailedException(exception: GitHubHttpRequestFailedException): ResponseEntity<ErrorResponse> {
-        return ResponseEntity.status(exception.statusCode).body(ErrorResponse(exception.statusCode, exception.errorMessage))
+        return ResponseEntity
+            .status(exception.statusCode)
+            .body(ErrorResponse(exception.statusCode, exception.errorMessage))
+    }
+
+    @ExceptionHandler
+    fun handleHttpMediaTypeNotAcceptableException(exception: HttpMediaTypeNotAcceptableException): ResponseEntity<ErrorResponse> {
+        return ResponseEntity
+            .status(exception.statusCode)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(ErrorResponse(exception.statusCode.value(), exception.message ?: ""))
     }
 }
